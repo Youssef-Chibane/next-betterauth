b087639659895cc79d2e63449ad83cc43900d739
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 18:05:57 +0100

    dobule layer auth verification

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,14 @@
 import LoginForm from "@/components/auth/LoginForm";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    redirect("/dashboard");
+  }
   return <LoginForm />;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,15 @@
 import SignOutButton from "@/components/auth/SignOutButton";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Hello From Dashboard Page</h1>
