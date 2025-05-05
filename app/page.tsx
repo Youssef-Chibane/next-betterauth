a3eeb6d8d6fb926b492dee616f7c5c5a2e674a1b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 18:15:09 +0100

    conditional rendering button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
       <h1 className="text-2xl font-bold">Better-Auth with Nextjs Demo</h1>
-      <Link href="/login">
-        <Button>Get Started</Button>
-      </Link>
+
+      {session ? (
+        <Link href="/login">
+          <Button>Dashboard</Button>
+        </Link>
+      ) : (
+        <Link href="/login">
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 }
