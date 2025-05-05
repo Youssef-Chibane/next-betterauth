8db482cd2b00e3e6c45183988d568da3498a9069
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 18:15:28 +0100

    printing logged user name

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,9 +10,12 @@ export default async function Page() {
   if (!session) {
     redirect("/login");
   }
+
+  const userName = session.user.name;
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello From Dashboard Page</h1>
+      <h1 className="text-4xl font-bold">Hello {userName}!</h1>
+      <p className="text-lg text-gray-600">Welcome to your dashboard.</p>
       <SignOutButton />
     </div>
   );
