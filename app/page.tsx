eb5bfab6e906ee62b94a56b94f20a3cba8178252
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 14:10:39 +0100

    adding get started button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">Better-Auth with Nextjs Demo</h1>
+      <Link href="/login">
+        <Button>Get Started</Button>
+      </Link>
+    </div>
+  );
 }
