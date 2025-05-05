fdbf5feb647a68dd49dedc9b0add4832b23edeb1
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 06:54:54 +0100

    install and generate prisma client

---
+++ lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "@/lib/prisma"
+ 
+
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+        provider: "sqlite", // or "mysql", "postgresql", ...etc
+    }),
+});
