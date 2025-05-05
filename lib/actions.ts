6172376a5551bba6f6818fe29a09ee14942f729d
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 15:49:55 +0100

    signin server action

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,6 +8,39 @@ interface State {
   errorMessage?: string | null;
 }
 
+export async function signIn(prevState: State, formdata: FormData) {
+  const rawFormData = {
+    email: formdata.get("email") as string,
+    password: formdata.get("pwd") as string,
+  };
+
+  const { email, password } = rawFormData;
+
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+    console.log("Signed in");
+  } catch (error) {
+    if (error instanceof APIError) {
+      switch (error.status) {
+        case "UNAUTHORIZED":
+          return { errorMessage: "User Not Found." };
+        case "BAD_REQUEST":
+          return { errorMessage: "Invalid email." };
+        default:
+          return { errorMessage: "Something went wrong." };
+      }
+    }
+    console.error("sign in with email has not worked", error);
+    throw error;
+  }
+  redirect("/dashboard");
+}
+
 export async function signUp(prevState: State, formdata: FormData) {
   const rawFormData = {
     email: formdata.get("email") as string,
