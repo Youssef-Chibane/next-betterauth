4f98c89849536333940d8977ae07bcf503b0a60e
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-05 07:26:21 +0100

    better auth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,30 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { db } from "@/lib/prisma"
- 
+import { db } from "@/lib/prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(db, {
+    provider: "sqlite", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 128,
+    autoSignIn: true,
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
