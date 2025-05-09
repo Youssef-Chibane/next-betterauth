79c1e75c55877204135a34adf869a14dc37d7580
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-09 17:52:48 +0100

    fixed prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
@@ -6,4 +6,4 @@ export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export const db = prisma
+export const db = prisma;
